8d22c59c3b829fddd8ceaf8566ef4980d6cb87ed
JondalarRodriguez <170204025@itsescarcega.edu.mx>
2021-03-23 11:20:56 -0600

    Agregado de componentes y fuentes de estilo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { CreditoComponent } from './credito/credito.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { VentasComponent } from './ventas/ventas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: PrincipalComponent},
+  {path: "Clientes", component: ClientesComponent},
+  {path: "Credito", component: CreditoComponent},
+  {path: "Login", component: LoginComponent},
+  {path: "Ventas", component: VentasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { CreditoComponent } from './credito/credito.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { NavComponent } from './Parciales/nav/nav.component';
+import { FooterComponent } from './Parciales/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    CreditoComponent,
+    VentasComponent,
+    NavComponent,
+    FooterComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
