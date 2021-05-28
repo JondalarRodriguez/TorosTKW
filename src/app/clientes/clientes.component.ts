41e53d99cdb9971ced23c3575962b7bf38f261fb
JOSÉ ANTONIO GERÓNIMO CU <joseant_125@hotmail.com>
2021-05-28 01:38:51 -0500

    otro commit

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -23,14 +23,27 @@ export class ClientesComponent implements OnInit {
     Mensualidad: new FormControl('')
   });
 
+  public clienteFormActualizar = new FormGroup({
+    Folio: new FormControl(''),
+    RGI: new FormControl(''),
+    Nombre: new FormControl(''),
+    FechaIngreso: new FormControl(''),
+    Direccion: new FormControl(''),
+    Telefono: new FormControl(''),
+    Edad: new FormControl(''),
+    Horario: new FormControl(''),
+    Clase: new FormControl(''),
+    Mensualidad: new FormControl('')
+  });
+
   public clientes: any = [];
-  
+
   constructor(private router: Router,
-              private sesioncliente: ClientesService) { }
+    private sesioncliente: ClientesService) { }
 
   ngOnInit(): void {
     this.sesioncliente.getCliente().subscribe(
-      (resp) =>{
+      (resp) => {
         console.log(resp);
         this.clientes = resp;
         console.log(this.clientes);
@@ -38,36 +51,41 @@ export class ClientesComponent implements OnInit {
     );
   }
 
-  public direccionCredito(){
+  public direccionCredito() {
     this.router.navigate(['credito']);
 
   }
-/*
-   public obtenerClientes(){
-     console.log("obtener cliente");
-     this.sesioncliente.getCliente().subscribe(
-       data =>{
-         console.log(data);
-       }
-     );
-   }
-*/
+  /*
+     public obtenerClientes(){
+       console.log("obtener cliente");
+       this.sesioncliente.getCliente().subscribe(
+         data =>{
+           console.log(data);
+         }
+       );
+     }
+  */
 
-public nuevoCliente(form: String){
-  this.sesioncliente.PostCliente(form).subscribe(
-    data =>{
-      if(data.status == 200){
-        alert("Datos guardados exitosmente");
-        
-      }else{
-        alert("Los datos no se pudieron guardr");
+  public nuevoCliente(form: String) {
+    this.sesioncliente.PostCliente(form).subscribe(
+      data => {
+        if (data.status == 200) {
+          alert("Datos guardados exitosmente");
+
+        } else {
+          alert("Los datos no se pudieron guardr");
+        }
+        console.log(data);
+      },
+      error => {
+        console.log(error);
       }
-      console.log(data);
-    },
-    error =>{
-      console.log(error);
-    }
-  );
-  
-}
+    );
+
+  }
+
+
+  public actualizarCliente(form: String){
+    console.log(form);
+  }
 }
