303ed4f454a088cf136c92dffe7f227a782b0515
flaviomorenosalvador75 <flavio_moreno_salvador@hotmail.com>
2021-06-05 19:57:05 -0500

    api

--- src/app/ventas/ventas.component.ts
+++ src/app/ventas/ventas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClientesService } from '../services/clienteservice/clientes.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ventas',
@@ -6,10 +9,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ventas.component.css']
 })
 export class VentasComponent implements OnInit {
+  public inventarioForm = new FormGroup({
+    folio: new FormControl(''),
+    Nombre: new FormControl('', Validators.required),
+    Costo: new FormControl('', Validators.required),
+    Precio: new FormControl('', Validators.required),
+    Existencia: new FormControl('', Validators.required)
+  });
+/*
+  public inventarioFormActualizar = new FormGroup({
+    folio: new FormControl(''),
+    Nombre: new FormControl(''),
+    Costo: new FormControl(''),
+    Precio: new FormControl(''),
+    Existencia: new FormControl('')
+  });
+*/
+  public productos: any = [];
+  public forUpdate: any = [];
+  public folioMayorString = "";
+  public folioNumero: number = 0;
+  public Busqueda: string = "";
 
-  constructor() { }
+  
+  constructor(private router: Router,
+    private sesioncliente: ClientesService) { }
 
   ngOnInit(): void {
+    this.sesioncliente.getCliente().subscribe(
+      (resp) => {
+        this.productos = resp;
+          this.nextFolio();
+      }
+    );
+  }
+  
+  public nextFolio() {
+    let element;
+    for (let index = 0; index < this.productos.length; index++) {
+      element = this.productos[index];
+      let folioObtener = element.folio;
+      if (folioObtener < element.folio) {
+        this.folioMayorString = element.folio;
+      }else{
+        this.folioMayorString = folioObtener
+      }
+    }
+
+    this.folioNumero = parseInt(this.folioMayorString);
+    this.folioNumero += 1;
+    this.folioMayorString = String(this.folioNumero);
+  }
+
+  public nuevoProducto(form: String) {
+
+    this.sesioncliente.PostCliente(form).subscribe(
+      data => {
+        if (data.status == 200) {
+          alert("Datos guardados exitosmente");
+          location.reload();
+        } else {
+          alert("Los datos no se pudieron guardar");
+        }
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
   }
 
 }
